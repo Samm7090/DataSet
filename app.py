6c4135a060ddc681e5f4e4ec57423a561b06fa22
Sammedh Nale <sammedh8nale@gmail.com>
2025-09-06 08:48:09 +0530

    Final changes

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def predict():
 if __name__ == "__main__":
     app.run(debug=True)'''
 
-from flask import Flask, render_template, request
+'''from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 import os
@@ -111,4 +111,54 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+'''
 
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd   
+import os
+import numpy as np
+
+app=Flask(__name__)
+
+base_dir=os.path.dirname(os.path.abspath(__file__))
+model_path=os.path.join(base_dir,'cyber_bullies.pkl')
+encoder_path=os.path.join(base_dir,'label_encode.pkl')
+
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
+
+with open(encoder_path, "rb") as f:
+    le = pickle.load(f)
+
+df=pd.read_csv('cyberbullying_tweets.csv')
+feature_names=df.drop(columns=['cyberbullying_type']).columns.tolist()
+
+@app.route("/")
+def home():
+    return render_template("index.html",feature_names=feature_names)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Get form values (convert to float)
+        text_input = request.form["tweet_text"]
+        # features = [float(x) for x in request.form.values()]
+        # final_features = np.array([features])   # shape (1, n_features)
+
+        # Predict
+        prediction = model.predict([text_input])       # numeric
+        decoded_pred = le.inverse_transform(prediction)  # decoded label
+
+        return render_template(
+            "index.html",
+            feature_names=feature_names,
+            prediction_text=f"Cyber Bullying Type is: {decoded_pred[0]}"
+        )
+
+    except Exception as e:
+        return render_template("index.html",feature_names=feature_names, prediction_text=f"Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
